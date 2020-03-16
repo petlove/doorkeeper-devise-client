f1c695cb401f10d999ffe937ecd7198728477701
linqueta <lincolnrodrs@gmail.com>
2020-03-16 17:11:36 -0300

    set environments

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module DoorkeeperDeviseClient
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    ENV["DOORKEEPER_APP_ID"] = 'oTr9w7kRN6K2C_nKzijV4MvnYPQu65Vdh_pMJ3R5d90'
+    ENV["DOORKEEPER_APP_SECRET"] = 'npmMxremg_vgbIHEOS8B-ThPAuqVxzhyjS3TFdGyFXQ'
+    ENV["DOORKEEPER_APP_URL"] = 'http://localhost:3000'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
